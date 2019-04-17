3afb97599c16074df740bca9569d7dc1a14cd2f3
e50jaywe <976870729@qq.com>
2019-04-17 10:49:41 +0800

    第一次提交

---
+++ front/assets/js/commomCss.js
@@ -0,0 +1,16 @@
+
+document.write('<link href="assets/css/bootstrap/bootstrap.min.css" rel="stylesheet" type="text/css">');
+
+document.write('<link href="assets/css/bootstrap/bootstrap-theme.min.css" rel="stylesheet" type="text/css">');
+
+document.write('<link href="assets/css/public/style.css" rel="stylesheet" type="text/css">');
+
+document.write('<link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">');
+
+document.write('<link href="assets/ionicons/css/ionicons.min.css" rel="stylesheet" type="text/css">');
+
+document.write('<link href="assets/css/public/flexslider.css" rel="stylesheet" type="text/css">');
+
+document.write('<link href="assets/css/public/owl.carousel.css" rel="stylesheet" type="text/css">');
+
+document.write('<link href="assets/css/public/magnific-popup.css" rel="stylesheet" type="text/css">');
